f95a238633dd4e0faac403426a8f31b672b50bca
NicholasKung <Nickung1212@gmail.com>
2020-03-06 14:32:28 -0500

    Created score container

---
+++ app/javascript/react/components/ScoreContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ScoreForm from './ScoreForm'
+
+const ScoreContainer = (props) => {
+
+
+  return (
+    <div>
+      <ScoreForm/>
+    </div>
+  )
+}
+
+
+export default ScoreContainer
