208da5b284cce58bbcdae560cfdbc77e3f6cefc5
NicholasKung <Nickung1212@gmail.com>
2020-03-18 15:18:57 -0400

    Added useEffect to display score

--- app/javascript/react/components/ScoreContainer.js
+++ app/javascript/react/components/ScoreContainer.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ScoreForm from './ScoreForm'
+import ScoreTile from './ScoreTile'
 
 const ScoreContainer = (props) => {
+
+  const [scores, setScores] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/scores.json')
+    .then(response => {
+      if(response.ok) {
+        return response
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setScores(body)
+    })
+    .catch(error => {
+      console.log(`Error fetching scores: ${error.message}`)
+    })
+  }, [])
+
+  const scoreTiles = scores.map((score) => {
+    return (
+      <ScoreTile
+        key={score.id}
+        scoreData={score}
+      />
+    )
+  })
 
 
   return (
     <div>
       <ScoreForm/>
+      {scoreTiles}
     </div>
   )
 }
