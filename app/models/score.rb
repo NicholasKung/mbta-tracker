451255269a1e56a8e3cd528af00bfa1de25f9038
NicholasKung <Nickung1212@gmail.com>
2020-03-19 09:52:23 -0400

    Added a validation for score not being higher than ten

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,13 @@ class Score < ApplicationRecord
   validates :first, numericality:true
   validates :second, numericality:true
   validates :third, numericality:true
+
+  validate :valid_score, :if => :valid_score?
+
+  def valid_score?
+    score_total = first + second + third
+    if score_total > 10
+      errors.add("score not valid because above 10")
+    end
+  end
 end
