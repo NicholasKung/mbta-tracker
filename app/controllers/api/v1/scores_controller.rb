8d6f3618aa62d4c6898e41a394e63c5778ba3a4b
NicholasKung <Nickung1212@gmail.com>
2020-03-24 13:28:22 -0400

    Total score calculator

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -4,4 +4,13 @@ class Api::V1::ScoresController < ApplicationController
     render json: Score.all
   end
 
+  def create
+    score = Score.new(score_params)
+    if score.save
+      render json: { score: score }
+    else
+      render json: { error: score.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
 end
